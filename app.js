cfe8bd16e9ecfccaacb02e37f5d993c3a1377a38
Bindholt <Cbindholt@gmail.com>
2023-04-12 11:20:58 +0200

    Ændret højde på den første søjle

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+
+window.addEventListener("load", init);
+
+function init() {
+    const bars = document.querySelectorAll(".bar");
+
+    const firstBar = bars[0];
+
+    const h = 50;
+
+    firstBar.style.height = `${h}px`;
+}
